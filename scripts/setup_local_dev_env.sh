2a0e0d28b0a438d16cb963557c6c160b3cca9e56
AaronYoung5 <aarony3131@gmail.com>
2021-02-07 16:38:11 -0600

    Updated script

--- scripts/setup_local_dev_env.sh
+++ scripts/setup_local_dev_env.sh
@@ -1,19 +1,100 @@
-#!/bin/sh
+#!/bin/bash
 
 # Sets up a local development environment using conda
 
-if ! command -v conda &> /dev/null
-then
-	echo "Please install anaconda and rerun this script"
-	exit
+check_command() {
+	cmd=$1
+	if ! command -v $cmd &> /dev/null
+	then
+		exit_error "Please install $cmd and rerun this script"
+	fi
+}
+
+exit_error() {
+	msg=$1
+	if [ -z "$1" ]; then
+		msg="Unknown Error"
+	fi
+	echo -e "\nScript exited with error: $msg"
+	exit 1
+}
+
+exit_okay() {
+	msg=$1
+	if [ -z "$msg" ]; then
+		msg="OK"
+	fi
+	echo -e "\nScript exitted: $msg"
+	exit 0
+}
+
+ask_okay() {
+	msg=$1
+	if [ -z "$msg" ]; then
+		exit_error "Pass a message to the ask_okay function."
+	fi
+
+	read -p "$msg ([y]/n)? " CONT
+	case ${CONT:0:1} in
+	y | Y | "")
+		return 0
+		;;
+	*)
+		return -1
+		;;
+	esac
+}
+
+ask_response() {
+	msg=$1
+	if [ -z "$msg" ]; then
+		exit_error "Pass a message to the ask_okay function."
+	fi
+
+	read -p "$msg " RESP
+	echo $RESP
+}
+
+check_command conda
+check_command curl
+check_command git
+
+# Verify the script was run on purpose
+if ask_okay "Create new conda env"; then
+	CREATE_NEW_CONDA_ENV=1
+elif ask_okay "Update conda env"; then
+	UPDATE_CONDA_ENV=1
+else
+	exit_okay
 fi
 
 # Clone the repo
-git clone -b develop https://github.com/WisconsinAutonomous/WisconsinAutonomous.github.io.git
-cd WisconsinAutonomous.github.io
+if ask_okay "Clone WisconsinAutonomous.github.io in $PWD"; then
+	git clone -b develop https://github.com/WisconsinAutonomous/WisconsinAutonomous.github.io.git
+	cd WisconsinAutonomous.github.io
+elif [[ "$(basename $(git remote show -n origin | grep Fetch | cut -d: -f2-))" != "WisconsinAutonomous.github.io" ]]; then
+	exit_error "Please WisconsinAutonomous.github.io or say yes to the previous question"
+fi
 
-# Create the conda environment
-conda env create -f environment.yml
+
+# Get the environment.yml file from github
+env_file=$(curl -fsSL https://raw.githubusercontent.com/WisconsinAutonomous/WisconsinAutonomous.github.io/master/environment.yml)
+
+# Check the name
+name="wa"
+if ! ask_okay "Environment name '$name' okay"; then
+	name=$(ask_response "Environment name :: ")
+fi
+
+# Create the conda environment from the retrieved environment.yml file
+tmpfile=temp_env.yml
+echo "$env_file" >>$tmpfile
+if [ -n "$CREATE_NEW_CONDA_ENV" ]; then
+	conda env create --name=$name -f=$tmpfile
+elif [ -n "$UPDATE_CONDA_ENV" ]; then
+	conda env update --name=$name -f=$tmpfile
+fi
+rm -f $tmpfile
 
 # Patch gem/ruby/bundle
 cd $(gem environment gemdir)
